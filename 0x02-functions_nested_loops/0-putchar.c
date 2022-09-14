66df344dbd4d2320e3bc8b8a145989c0904e6d0d
Tagoe William <wi0273613223@gmail.com>
2022-09-14 16:24:21 -0700

    New

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+_putchar ('G');
+_putchar('o');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('f');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+ 
+ return (0); 
+
+}
